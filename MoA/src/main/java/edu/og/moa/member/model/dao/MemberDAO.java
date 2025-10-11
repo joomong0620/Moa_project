f6a12e43b387a9703603dd3a2bde1a1093fcbddd
ttttww55 <field56@naver.com>
2025-10-11 23:39:26 +0900

    10/11 로그인 및 회원가입

--- MoA/src/main/java/edu/og/moa/member/model/dao/MemberDAO.java
+++ MoA/src/main/java/edu/og/moa/member/model/dao/MemberDAO.java
@@ -12,9 +12,16 @@ public class MemberDAO {
 	@Autowired
 	private MemberMapper memberMapper;
 
+	// 회원 가입
 	public int signUp(Member inputMember) {
 		
 		return memberMapper.signUp(inputMember);
+	}
+	
+	// 로그인 서비스
+	public Member login(Member inputMember) {
+		
+		return memberMapper.login(inputMember);
 	}
 
 	

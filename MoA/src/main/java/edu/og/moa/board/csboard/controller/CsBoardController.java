63baf920543380644f057231c4b3c55ceac00f8d
TaeWoong <field56@naver.com>
2025-10-13 16:47:03 +0900

    10/13 csboard

--- MoA/src/main/java/edu/og/moa/board/csboard/controller/CsBoardController.java
+++ MoA/src/main/java/edu/og/moa/board/csboard/controller/CsBoardController.java
@@ -3,15 +3,17 @@ package edu.og.moa.board.csboard.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import edu.og.moa.board.csboard.model.service.CsService;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@RequestMapping("/board")
+@RequestMapping("/board/question")
 @SessionAttributes("loginMember")
 @Slf4j
 public class CsBoardController {
@@ -21,22 +23,30 @@ public class CsBoardController {
 	
 	
 	// 자주 묻는 질문으로 화면 이동
-	@GetMapping("/question")
+	@GetMapping("/board/{communityCode:[5]+}")
 	public String question() {
 		
-		return "board/csboard/question";
+		return "board/csboard/5";
 				
 	}
 	
-	// 내 문의 내역으로 화면 이동
-	@GetMapping("/question/questionList")
-	public String questionList() {
+	// 내 문의 내역 게시판 조회
+	@GetMapping("/board/{communityCode:[5]+}/questionList/{qCode:[1-3]+}")
+	public String questionList(@PathVariable("communityCode") int communityCode,
+			@RequestParam(value="cp", required=false, defaultValue="1") int cp,
+			
+			
+			) {
+		
+		
+		
+		
 		return "board/csboard/questionList";
 	}
 	
-	// 내 문의 내역 게시판 조회
-	@PostMapping("/question/questionList")
-	public String questionList() {
+	
+		
+		
 		
 		
 		

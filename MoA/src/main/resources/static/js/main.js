944925d7f380bfe1f38d606a01bb51a139611e75
Soyeon <homepot@naver.com>
2025-10-13 17:23:17 +0900

    Feat. 1013 전시 목록조회 --> 상세조회 까지 가능 배너 안뜨는 이슈 수정

---
+++ MoA/src/main/resources/static/js/main.js
@@ -0,0 +1,39 @@
+let slideIndex = 0;
+let slideTimer;
+
+showSlides();
+
+// 계산식
+function showSlides() {
+  const slides = document.getElementsByClassName("banner-slide");
+  const dots = document.getElementsByClassName("dot");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
+
+  // 5초마다 자동 변경
+  slideTimer = setTimeout(showSlides, 5000);
+}
+
+function plusSlides(n) {
+  clearTimeout(slideTimer);
+  slideIndex += n - 1;
+  showSlides();
+}
+
+function currentSlide(n) {
+  clearTimeout(slideTimer);
+  slideIndex = n - 1;
+  showSlides();
+}

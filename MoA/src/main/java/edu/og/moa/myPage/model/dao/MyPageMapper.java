756e58559b5eb04d1765322f44c483eea35b5169
joomong0620 <yeon062000@naver.com>
2025-10-15 17:34:52 +0900

    Feat. 마이페이지 내 예매내역

--- MoA/src/main/java/edu/og/moa/myPage/model/dao/MyPageMapper.java
+++ MoA/src/main/java/edu/og/moa/myPage/model/dao/MyPageMapper.java
@@ -1,8 +1,12 @@
 package edu.og.moa.myPage.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import edu.og.moa.board.freeboard.model.dto.Board;
 import edu.og.moa.member.model.dto.Member;
+import edu.og.moa.pay.model.dto.Payment;
 
 @Mapper
 public interface MyPageMapper {
@@ -22,6 +26,19 @@ public interface MyPageMapper {
 
 	// 회원 탈퇴	
 	int secession(int memberNo);
+
+	// 좋아요한 게시물
+	List<Board> selectLikeBoard(int memberNo);
+
+
+	// 내가쓴 게시물
+	List<Board> selectMyBoardList(int memberNo);
+
+	// 결제(예매) 내역 조회
+	List<Payment> selectPaymentList(int memberNo);
+
+	// 예매 취소
+	int cancelPayment(String payNo);
 
 
 

9dbfb13e68a262b2ae74cea410e701d1012f9693
Soyeon <homepot@naver.com>
2025-10-15 13:13:33 +0900

    Feat. 1015 결제 수정

--- MoA/src/main/java/edu/og/moa/main/controller/MainController.java
+++ MoA/src/main/java/edu/og/moa/main/controller/MainController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import edu.og.moa.board.exhibition.model.dto.JsonBoardImage;
 import edu.og.moa.board.exhibition.model.service.ExhibitionService;
 import edu.og.moa.board.exhibition.model.service.JsonExhibitionService;
+import edu.og.moa.board.performance.model.dto.PerformanceBoardImage;
+import edu.og.moa.board.performance.model.service.PerformanceService;
 import edu.og.moa.member.model.dto.Member;
 
 @Controller
@@ -20,34 +22,30 @@ public class MainController {
 
     @Autowired
     private JsonExhibitionService jsonExhibitionService;
-    
-    @RequestMapping("/")
-    public String mainForward(
+
+    @Autowired
+    private PerformanceService performanceService;
+
+
+    @GetMapping("/")
+    public String mainPage(
         @SessionAttribute(value = "loginMember", required = false) Member loginMember,
         Model model
     ) {
-        if (loginMember != null) {
-            model.addAttribute("name", loginMember.getMemberNickname()); // 로그인 회원의 닉네임
-        } else {
-            model.addAttribute("name", null); // 비로그인 상태일 때
-        }
+        if (loginMember != null)
+            model.addAttribute("name", loginMember.getMemberNickname());
+        else
+            model.addAttribute("name", null);
+
+        // 전시 썸네일
+        List<JsonBoardImage> exhibitionList = jsonExhibitionService.selectExhibitionThumbnailList();
+        model.addAttribute("exhibitionList", exhibitionList);
+
+        // 공연 썸네일
+        List<PerformanceBoardImage> performanceList = performanceService.selectPerformanceList();
+        model.addAttribute("performanceList", performanceList);
 
         return "common/main";
     }
-    
-	
-
-    @GetMapping("/")
-    public String mainPage(Model model) {
-
-        // 전시 썸네일 리스트 가져오기
-        List<JsonBoardImage> exhibitionList = jsonExhibitionService.selectExhibitionThumbnailList();
-
-        // 모델에 담아서 main.html 로 전달
-        model.addAttribute("exhibitionList", exhibitionList);
-
-        return "common/main"; // templates/common/main.html
-    }
-
     
 }

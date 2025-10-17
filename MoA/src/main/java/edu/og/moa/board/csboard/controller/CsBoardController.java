8510b65cc50c27bb48a88a095efaf4a050f11c83
nickName333777 <yypark.rok@gmail.com>
2025-10-17 21:08:08 +0900

    2025/10/17작업

--- MoA/src/main/java/edu/og/moa/board/csboard/controller/CsBoardController.java
+++ MoA/src/main/java/edu/og/moa/board/csboard/controller/CsBoardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import edu.og.moa.board.csboard.model.service.CsService;
+//import edu.og.moa.board.csboard.model.service.CsService;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -20,40 +20,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CsBoardController {
 	
-	@Autowired
-	private CsService service;
-	
-	
-	// 자주 묻는 질문으로 화면 이동
-	@GetMapping("/{communityCode:5}")
-	public String question() {
+//	@Autowired
+//	private CsService service;
+//	
+//	
+//	// 자주 묻는 질문으로 화면 이동
+//	@GetMapping("/{communityCode:5}")
+//	public String question() {
+//		
+//		return "board/csboard/question";
+//				
+//	}
+//	
+//	// 내 문의 내역 게시판 조회
+//	@GetMapping("/{communityCode:5}/questionList/{qCode:[1-3]}")
+//	public String questionList(@PathVariable("communityCode") int communityCode,
+//			@RequestParam(value="cp", required=false, defaultValue="1") int cp,
+//			@PathVariable("qCode") int qCode, Model model
+//			
+//			
+//			) {
+//		
+//		Map<String, Object> questionList = service.selectQuestionList(communityCode, qCode, cp);
+//		
+//		model.addAttribute("communityCode", communityCode);
+//		
+//		
+//		model.addAttribute("questionList", questionList);
+//		
 		
-		return "board/csboard/question";
-				
-	}
-	
-	// 내 문의 내역 게시판 조회
-	@GetMapping("/{communityCode:5}/questionList/{qCode:[1-3]}")
-	public String questionList(@PathVariable("communityCode") int communityCode,
-			@RequestParam(value="cp", required=false, defaultValue="1") int cp,
-			@PathVariable("qCode") int qCode, Model model
-			
-			
-			) {
-		
-		Map<String, Object> questionList = service.selectQuestionList(communityCode, qCode, cp);
-		
-		model.addAttribute("communityCode", communityCode);
-		
-		
-		model.addAttribute("questionList", questionList);
-		
-		
-		return "board/csboard/questionList";
-	}
-	
-	
-		
+//		return "board/csboard/questionList";
+//	}
+//	
+//	
+//		
 		
 		
 		
